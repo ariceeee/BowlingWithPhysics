8bbb0a7ff4c46200687bd7234bc2ad133ae21b7c
Ariana Rice <69221845+ariceeee@users.noreply.github.com>
2025-02-11 11:54:07 -0800

    Player movement

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,8 +1,10 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     public UnityEvent OnSpacePressed = new UnityEvent();
 
     void Start()
@@ -16,5 +18,18 @@ public class InputManager : MonoBehaviour
         {
             OnSpacePressed?.Invoke();
         }
+
+        Vector2 input = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            input += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            input += Vector2.right;
+        }
+
+        OnMove?.Invoke(input);
     }
 }
